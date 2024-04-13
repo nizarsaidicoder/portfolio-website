c899b28a79bffbb0801f5224cdb4af5cf99258b2
nizar <nizar.saidi.coder@gmail.com>
2024-04-14 00:14:43 +0200

    Implemented the hiremeSection UI

---
+++ components/HiremeSection.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    id="hireme-section"
+    class="section hireme--section">
+    <div class="hireme--container">
+      <h1 class="hireme--title highlight">Hire Me</h1>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
